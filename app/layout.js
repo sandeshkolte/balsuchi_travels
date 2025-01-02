c85ad3874b3c772b17b695c15acab3bae585fb28
Sandesh Kolte <sandeshkolte11@gmail.com>
2025-01-02 22:17:28 +0530

    google

--- app/layout.js
+++ app/layout.js
@@ -24,8 +24,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${poppins.className} antialiased`}
-      >
+        className={`${poppins.className} antialiased`}>
         {children}
       </body>
     </html>
